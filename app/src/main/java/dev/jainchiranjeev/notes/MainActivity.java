65685dc2e16b4620944470295a64eebba6f9740b
JChiranjeev <cjain252@gmail.com>
2020-05-09 12:26:50 +0530

    Added DayNight theme with colors to compliment. Added Raleway fonts. Made StatusBar transparent. Made NavigationBar transparent upto API 28 (Pie)

--- app/src/main/java/dev/jainchiranjeev/notes/MainActivity.java
+++ app/src/main/java/dev/jainchiranjeev/notes/MainActivity.java
@@ -1,8 +1,11 @@
 package dev.jainchiranjeev.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        getWindow().setBackgroundDrawableResource(R.drawable.background_gradient_main_activity);
     }
 }
